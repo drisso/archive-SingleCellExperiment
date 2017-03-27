cc1c54ed6e965080b6638d9a69b0f12087322f41
Aaron Lun <alun@wehi.edu.au>
2017-03-27 19:35:32 +0100

    Added support for packed symmetric matrices.

--- tests/testthat/test-numeric.R
+++ tests/testthat/test-numeric.R
@@ -131,6 +131,25 @@ expect_identical(test.mat, .Call(beachmat:::cxx_test_numeric_access, A, 1L))
 expect_identical(test.mat, .Call(beachmat:::cxx_test_numeric_access, A, 2L))
 expect_identical(test.mat, .Call(beachmat:::cxx_test_numeric_access, A, 3L))
 
+# Testing dense symmetric matrices (dspMatrix):
+
+library(beachmat); library(testthat); library(Matrix)
+
+set.seed(45678)
+A <- pack(forceSymmetric(matrix(as.double(rpois(100, lambda=5)), 10, 10)))
+test.mat <- as.matrix(A)
+dimnames(test.mat) <- NULL
+expect_identical(test.mat, .Call(beachmat:::cxx_test_numeric_access, A, 1L))
+expect_identical(test.mat, .Call(beachmat:::cxx_test_numeric_access, A, 2L))
+expect_identical(test.mat, .Call(beachmat:::cxx_test_numeric_access, A, 3L))
+
+A <- pack(forceSymmetric(matrix(as.double(rpois(100, lambda=5)), 20, 20), "L"))
+test.mat <- as.matrix(A)
+dimnames(test.mat) <- NULL
+expect_identical(test.mat, .Call(beachmat:::cxx_test_numeric_access, A, 1L))
+expect_identical(test.mat, .Call(beachmat:::cxx_test_numeric_access, A, 2L))
+expect_identical(test.mat, .Call(beachmat:::cxx_test_numeric_access, A, 3L))
+
 # Testing HDF5 matrices:
 
 set.seed(34567)
