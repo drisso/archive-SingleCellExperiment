b0e1383837071999e18da6eaf7afd925fbe6868c
Aaron Lun <alun@wehi.edu.au>
2017-04-16 14:14:59 +0100

    Added HDF5 flags at the R level. Modified configure.ac to set R/C++ flags. configure uses R build system, added cleanup. Removed HDF5 dependency in vignette, DESCRIPTION.

--- R/zzz.R
+++ R/zzz.R
@@ -7,17 +7,17 @@ pkgconfig <- function(opt = c("PKG_LIBS", "PKG_CPPFLAGS"))
         arch <- ""
     }
     patharch <- paste0(path, arch)
+    extralib <- ifelse(use.hdf5, "-lhdf5_cpp", "")
 
     result <- switch(match.arg(opt), PKG_CPPFLAGS={
         sprintf('-I"%s"', system.file("include", package="beachmat"))
     }, PKG_LIBS={
         switch(Sys.info()['sysname'], Linux={
-            sprintf('-L%s -Wl,-rpath,%s -lbeach -lhdf5_cpp -pthread',
-                    patharch, patharch)
+            sprintf('-L%s -Wl,-rpath,%s -lbeach %s -pthread', patharch, patharch, extralib)
         }, Darwin={
-            sprintf('%s/libbeach.a -lhdf5_cpp -pthread', patharch)
+            sprintf('%s/libbeach.a %s -pthread', patharch, extralib)
         }, Windows={
-            sprintf('-L"%s" -lbeach -lhdf5_cpp -pthread -lws2_32', patharch)
+            sprintf('-L"%s" -lbeach %s -pthread -lws2_32', patharch, extralib)
         }
     )})
 
