2e04c1b37c9ad6552f63d8c87b89eabdf0db042b
Aaron Lun <alun@wehi.edu.au>
2017-04-24 10:34:57 +0100

    Added tests for SCE construction, get/setters.

---
+++ tests/testthat/test-methods.R
@@ -0,0 +1,60 @@
+# Checks for proper functioning of the methods.
+# library(beachmat); library(testthat)
+
+set.seed(1000)
+ncells <- 100
+v <- matrix(rnorm(20000), ncol=ncells)
+sce <- SingleCellExperiment(assay=v)
+
+# Adding spike-ins.
+is.spike1 <- rbinom(nrow(v), 1, 0.2)==1
+isSpike(sce, "ERCC") <- is.spike1
+expect_identical(spikeNames(sce), "ERCC")
+expect_identical(isSpike(sce, "ERCC"), is.spike1)
+expect_identical(isSpike(sce), is.spike1)
+
+is.spike2 <- rbinom(nrow(v), 1, 0.3)==1
+isSpike(sce, "SIRV") <- is.spike2
+expect_identical(spikeNames(sce), c("ERCC", "SIRV"))
+expect_identical(isSpike(sce, "ERCC"), is.spike1) # check still the same.
+expect_identical(isSpike(sce, "SIRV"), is.spike2)
+expect_identical(isSpike(sce), is.spike1 | is.spike2)
+
+isSpike(sce, "ERCC") <- NULL
+expect_identical(spikeNames(sce), "SIRV")
+expect_error(isSpike(sce, "ERCC"), "spike-in set 'ERCC' does not exist")
+expect_identical(isSpike(sce, "SIRV"), is.spike2)
+expect_identical(isSpike(sce), is.spike2)
+
+chosen <- sample(nrow(v), 20) # integer setting
+isSpike(sce, "ERCC") <- chosen
+expect_identical(which(isSpike(sce, "ERCC")), sort(chosen)) 
+expect_identical(spikeNames(sce), c("SIRV", "ERCC")) # flipped
+
+rownames(sce) <- paste0("Gene", seq_len(nrow(v))) # character setting
+isSpike(sce, "SIRV") <- rownames(sce)[chosen]
+expect_identical(which(isSpike(sce, "SIRV")), sort(chosen))
+
+beachmat:::int_metadata(sce)$spike_names <- c("random")
+expect_error(validObject(sce), "no field specifying rows belonging to spike-in set 'random'", fixed=TRUE)
+
+# Adding size factors.
+sf1 <- 2^rnorm(ncells)
+sizeFactors(sce) <- sf1
+expect_identical(sizeFactors(sce), sf1)
+
+sf2 <- 2^rnorm(ncells, sd=2)
+sizeFactors(sce, "ERCC") <- sf2
+expect_identical(sizeFactors(sce), sf1) # check still the same
+expect_identical(sizeFactors(sce, "ERCC"), sf2)
+
+sizeFactors(sce) <- NULL
+expect_identical(sizeFactors(sce), NULL)
+expect_identical(sizeFactors(sce, "ERCC"), sf2) # check still the same
+
+sizeFactors(sce, "ERCC") <- NULL
+expect_identical(sizeFactors(sce, "ERCC"), NULL)
+
+# Checking package version.
+expect_identical(objectVersion(sce), packageVersion("beachmat"))
+
---
+++ tests/testthat/test-sce.R
@@ -0,0 +1,81 @@
+# Checks for proper construction and get/setting of the slots of a SingleCellExperiment.
+# library(beachmat); library(testthat)
+
+set.seed(1000)
+ncells <- 100
+
+# Adding assays.
+v <- matrix(rnorm(20000), ncol=ncells)
+sce <- SingleCellExperiment(assay=v)
+expect_equivalent(assay(sce), v)
+
+u <- matrix(rpois(20000, 5), ncol=ncells)
+sce <- SingleCellExperiment(assay=list(counts=u, exprs=v))
+expect_equivalent(assay(sce, "counts"), u)
+expect_equivalent(assay(sce, "exprs"), v)
+
+w <- matrix(runif(20000), ncol=ncells)
+assay(sce, "exprs") <- w
+expect_equivalent(assay(sce, "exprs"), w)
+
+# Adding metadata.
+rd <- DataFrame(stuff=runif(nrow(v)))
+cd <- DataFrame(whee=runif(ncells))
+sce <- SingleCellExperiment(u, rowData=rd, colData=cd)
+expect_equal(rd, rowData(sce))
+expect_equal(cd, colData(sce))
+
+cextra <- rnorm(ncells)
+sce$blah <- cextra
+expect_equal(cextra, colData(sce)$blah)
+rextra <- rnorm(nrow(v))
+rowData(sce)$blah <- rextra
+expect_equal(rextra, rowData(sce)$blah)
+
+sce <- SingleCellExperiment(u, metadata=list(yay=1))
+expect_equal(metadata(sce)$yay, 1)
+metadata(sce)$yay <- "stuff"
+expect_identical(metadata(sce)$yay, "stuff")
+
+# Adding reduced dimensions.
+pca <- matrix(runif(ncells*5), ncells)
+tsne <- matrix(rnorm(ncells*2), ncells)
+combined <- SimpleList(PCA=pca, tSNE=tsne)
+sce <- SingleCellExperiment(u, reducedDims=combined)
+
+expect_equivalent(pca, reducedDim(sce, "PCA"))
+expect_equivalent(tsne, reducedDim(sce, "tSNE"))
+expect_equivalent(reducedDims(sce), combined)
+
+dm <- matrix(runif(ncells*2), ncells)
+reducedDim(sce, "DM") <- dm
+expect_equivalent(dm, reducedDim(sce, "DM"))
+reducedDim(sce, "DM") <- NULL 
+expect_equivalent(reducedDims(sce), SimpleList())
+reducedDims(sce) <- SimpleList(DM=dm)
+expect_equivalent(SimpleList(DM=dm), reducedDims(sce))
+
+expect_error(reducedDims(sce) <- SimpleList(dm), "'reducedDims' must be a named list", fixed=TRUE)
+expect_error(reducedDim(sce, "DM") <- dm[1:10,], "'nrow' of each element of 'reducedDims' is not equal to 'ncol(object)'", fixed=TRUE)
+expect_error(reducedDim(sce, "DM") <- "huh", "'nrow' of each element of 'reducedDims' is not equal to 'ncol(object)'", fixed=TRUE)
+
+# Checking internals.
+sce <- SingleCellExperiment(assay=u)
+expect_identical(nrow(beachmat:::int_elementMetadata(sce)), nrow(sce))
+expect_identical(nrow(beachmat:::int_colData(sce)), ncol(sce))
+expect_identical(length(beachmat:::int_metadata(sce)), 1L)
+
+beachmat:::int_elementMetadata(sce)$whee <- rextra
+expect_equal(rextra, beachmat:::int_elementMetadata(sce)$whee)
+beachmat:::int_elementMetadata(sce) <- DataFrame(1:5)
+expect_error(validObject(sce), "'nrow' of internal 'rowData' not equal to 'nrow(object)'", fixed=TRUE)
+
+beachmat:::int_colData(sce)$stuff <- cextra
+expect_equal(cextra, beachmat:::int_colData(sce)$stuff)
+beachmat:::int_colData(sce) <- DataFrame(1:5)
+expect_error(validObject(sce), "'nrow' of internal 'colData' not equal to 'ncol(object)'", fixed=TRUE)
+
+beachmat:::int_metadata(sce)$urg <- "I was here"
+expect_identical(beachmat:::int_metadata(sce)$urg, "I was here")
+
+
