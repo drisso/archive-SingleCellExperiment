302aefcbbd999e3a7ea3ee00ff0596ca5cf45590
Aaron Lun <alun@wehi.edu.au>
2017-04-30 21:24:41 +0100

    Enveloped all C++ functions in the 'beachmat' namespace. Switched installation subdirectories for headers, lib. Removed some unnecessary data members for HDF5_character_matrix.

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 pkgconfig <- function(opt = c("PKG_LIBS", "PKG_CPPFLAGS"))
 {
-    path <- system.file("libs", package="beachmat", mustWork=TRUE)
+    path <- system.file("lib", package="beachmat", mustWork=TRUE)
     if (nzchar(.Platform$r_arch)) {
         arch <- sprintf("/%s", .Platform$r_arch)
     } else {
@@ -13,11 +13,11 @@ pkgconfig <- function(opt = c("PKG_LIBS", "PKG_CPPFLAGS"))
         sprintf('-I"%s"', system.file("include", package="beachmat"))
     }, PKG_LIBS={
         switch(Sys.info()['sysname'], Linux={
-            sprintf('-L%s -Wl,-rpath,%s -lbeach %s -pthread', patharch, patharch, extralib)
+            sprintf('-L%s -Wl,-rpath,%s -lbeachmat %s -pthread', patharch, patharch, extralib)
         }, Darwin={
-            sprintf('%s/libbeach.a %s -pthread', patharch, extralib)
+            sprintf('%s/libbeachmat.a %s -pthread', patharch, extralib)
         }, Windows={
-            sprintf('-L"%s" -lbeach %s -pthread -lws2_32', patharch, extralib)
+            sprintf('-L"%s" -lbeachmat %s -pthread -lws2_32', patharch, extralib)
         }
     )})
 
