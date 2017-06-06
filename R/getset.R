a729812c61abf248f0c39dc614cd090c5d973691
Davide Risso <dar2062@med.cornell.edu>
2017-06-06 11:27:15 -0400

    Add coercion from SummarizedExperiment

--- R/getset.R
+++ R/getset.R
@@ -1,16 +1,16 @@
 # Official getter/setter functions.
 
-setMethod("reducedDims", "SingleCellExperiment", function(x) { 
+setMethod("reducedDims", "SingleCellExperiment", function(x) {
     x@reducedDims
 })
 
 setReplaceMethod("int_reducedDims", "SingleCellExperiment", function(x, value) {
     x@reducedDims <- value
-    return(x)    
+    return(x)
 })
 
 setReplaceMethod("reducedDims", "SingleCellExperiment", function(x, value) {
-    for (i in seq_along(value)) { 
+    for (i in seq_along(value)) {
         if (!.not_reddim_mat(value[[i]], x)) { rownames(value[[i]]) <- colnames(x) }
     }
     int_reducedDims(x) <- value
@@ -54,7 +54,7 @@ setReplaceMethod("int_metadata", "SingleCellExperiment", function(x, value) {
 # Size factor getter/setter functions.
 
 setMethod("sizeFactors", "SingleCellExperiment", function(object, type=NULL) {
-    field <- .get_sf_field(type)          
+    field <- .get_sf_field(type)
     return(int_colData(object)[[field]])
 })
 
@@ -70,15 +70,15 @@ setReplaceMethod("sizeFactors", "SingleCellExperiment", function(object, type=NU
 
 setMethod("isSpike", c("SingleCellExperiment", "character"), function(x, type) {
     field <- .get_spike_field(type)
-    if (!type %in% spikeNames(x)) { 
+    if (!type %in% spikeNames(x)) {
         stop(sprintf("spike-in set '%s' does not exist", type))
     }
-    return(int_elementMetadata(x)[[field]])    
+    return(int_elementMetadata(x)[[field]])
 })
 
-for (sig in c("missing", "NULL")){ 
+for (sig in c("missing", "NULL")){
     setMethod("isSpike", c("SingleCellExperiment", sig), function(x, type) {
-        return(int_elementMetadata(x)[[.spike_field]])    
+        return(int_elementMetadata(x)[[.spike_field]])
     })
 }
 
@@ -94,21 +94,21 @@ setReplaceMethod("isSpike", c("SingleCellExperiment", "character"), function(x, 
         md$spike_names <- union(md$spike_names, type)
         rd[[field]] <- .convert_subset_spike(value, .length=nrow(x), .names=rownames(x))
     }
-    
+
     int_metadata(x) <- md
     int_elementMetadata(x) <- rd # need this, otherwise 'isSpike' below won't be up-to-date.
-    
+
     # Updating is_spike.
     all.spikes <- lapply(spikeNames(x), isSpike, x=x)
     combined.spikes <- Reduce("|", all.spikes)
     rd[[.spike_field]] <- combined.spikes
-    int_elementMetadata(x) <- rd  
+    int_elementMetadata(x) <- rd
     return(x)
 })
 
 # Other useful functions.
 
-setMethod("spikeNames", "SingleCellExperiment", function(x) { 
+setMethod("spikeNames", "SingleCellExperiment", function(x) {
     int_metadata(x)$spike_names
 })
 
@@ -116,3 +116,13 @@ setMethod("objectVersion", "SingleCellExperiment", function(x) {
     int_metadata(x)$version
 })
 
+setAs("SummarizedExperiment",
+      "SingleCellExperiment",
+      function(from) {
+        SingleCellExperiment(assays = assays(from),
+                             colData = colData(from),
+                             rowData = rowData(from),
+                             metadata = metadata(from)
+                             )
+      }
+)
